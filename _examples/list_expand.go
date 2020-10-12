e173a39ddbb860d362973d4a7511b6208c24a44c
namelessvoid <simon_kerler@web.de>
2020-10-12 21:46:23 +0200

    Try fixing scrolling for word wrapped lists

---
+++ _examples/list_expand.go
@@ -0,0 +1,74 @@
+// Copyright 2017 Zack Guo <zack.y.guo@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT license that can
+// be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"log"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+)
+
+func main() {
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	l := widgets.NewList()
+	l.Title = "List"
+	l.Rows = []string{}
+	l.TextStyle = ui.NewStyle(ui.ColorYellow)
+	l.WrapText = true
+	l.SetRect(0, 0, 25, 8)
+
+	ui.Render(l)
+
+	previousKey := ""
+	uiEvents := ui.PollEvents()
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		case "j", "<Down>":
+			l.ScrollDown()
+		case "k", "<Up>":
+			l.ScrollUp()
+		case "<C-d>":
+			l.ScrollHalfPageDown()
+		case "<C-u>":
+			l.ScrollHalfPageUp()
+		case "<C-f>":
+			l.ScrollPageDown()
+		case "<C-b>":
+			l.ScrollPageUp()
+		case "g":
+			if previousKey == "g" {
+				l.ScrollTop()
+			}
+		case "<Home>":
+			l.ScrollTop()
+		case "G", "<End>":
+			l.ScrollBottom()
+		case "a":
+			l.Rows = append(l.Rows, ">>>", "Something short")
+			l.ScrollBottom()
+		case "A":
+			l.Rows = append(l.Rows, ">>>", "This is something very long with breaks")
+			l.ScrollBottom()
+		}
+
+		if previousKey == "g" {
+			previousKey = ""
+		} else {
+			previousKey = e.ID
+		}
+
+		ui.Render(l)
+	}
+}
